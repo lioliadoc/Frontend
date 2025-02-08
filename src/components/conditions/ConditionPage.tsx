a1c020a7a2ae4e622ee49763e9c09dd4a96bc2a6
lioliadoc <liolia.doc@gmail.com>
2025-02-08 11:46:38 -0600

    ConditionListPage css modified

--- src/components/conditions/ConditionPage.tsx
+++ src/components/conditions/ConditionPage.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { Container, Row, Col, Spinner, Alert, Button } from 'react-bootstrap';
 import { useParams, useNavigate } from 'react-router-dom';
+import './ConditionListPage.css';
 
 const API_URL = import.meta.env.VITE_API_URL;
 
