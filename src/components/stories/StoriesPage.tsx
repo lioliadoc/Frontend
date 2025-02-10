c5078d68d05b8b543fca884c2368c8d802c34ce3
lioliadoc <liolia.doc@gmail.com>
2025-02-10 15:29:37 -0600

    endpoint google login modif

--- src/components/stories/StoriesPage.tsx
+++ src/components/stories/StoriesPage.tsx
@@ -157,7 +157,7 @@ function StoriesPage() {
               <p className="text-muted">You must log in to share a story.</p>
               <Button
                 variant="outline-primary"
-                onClick={() => window.location.href = `${API_URL}/login/force-login`}
+                onClick={() => window.location.href = `${API_URL}/login/google/authorized`}
             
               >
                 Log In via Google
